ead1a0248bf5ee8fe4da7484aaadd07d4503b1c5
Philip <bonny.philip@accenture.com>
2025-09-16 13:28:11 +0530

    complete code

---
+++ main.py
@@ -0,0 +1,8 @@
+# Dependencies : uv add langgraph langchain openai python-dotenv langchain-google-genai
+
+def main():
+    print("Hello from langgraph-agent!")
+
+
+if __name__ == "__main__":
+    main()
